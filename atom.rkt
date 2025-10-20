17936463de595598fa5f8dab7959b77cf451750b
ywang2 <yang.wang.2@hexagon.com>
2025-10-20 17:59:23 +0800

    atom and car and cdr and cons

---
+++ atom.rkt
@@ -0,0 +1,18 @@
+#lang racket
+
+(define (atom? x)
+  (and (not (pair? x)) (not (null? x)))
+)
+
+(eq? true (atom? 'a))
+(eq? true (atom? 123))
+(eq? false (atom? '()))
+(eq? false (atom? '(('a) 1)))
+
+(equal? '(1 2 3) (car '((1 2 3) 1)))
+(equal? 1 (car '(1 (2 3) 1)))
+
+(equal? '(1) (cdr '((1 2 3) 1)))
+(equal? '((2 3)) (cdr '(1 (2 3))))
+
+(equal? '(a b) (cons 'a '(b)))
