a9fdbb5552ad07959472e9d671094d5a2c231f0b
cafewang <434637878@qq.com>
2025-11-02 23:01:57 +0800

    more number ops

--- number_operation.rkt
+++ number_operation.rkt
@@ -65,3 +65,53 @@
 
 (equal? '(3 5 7) (tup-plus '(1 2 3) '(2 3 4)))
 (equal? '(6 2 3) (tup-plus '(1 2 3) '(5)))
+
+;works for non-negative numbers only
+(define > 
+  (lambda (x y)
+    (cond 
+      ((zero? x) #f)
+      ((zero? y) #t)
+      (else (> (sub1 x) (sub1 y))))))
+
+(eq? #f (> 5 5))
+(eq? #t (> 5 4))
+(eq? #f (> 4 5))
+
+(define divide
+  (lambda (x y)
+    (cond
+      ((< x y) 0)
+      (else (add1 (divide (- x y) y))))))
+
+(eq? 2 (divide 10 5))
+(eq? 0 (divide 4 5))
+(eq? 1 (divide 7 5))
+
+(define divide-and-remainder
+  (lambda (x y)
+    (cond
+      ((< x y) (list 0 x))
+      (else (cons (add1 (car (divide-and-remainder (- x y) y))) (cdr (divide-and-remainder (- x y) y)))))))
+
+(equal? '(3 2) (divide-and-remainder 17 5))
+(equal? '(1 0) (divide-and-remainder 17 17))
+(equal? '(0 2) (divide-and-remainder 2 5))
+
+(define length
+  (lambda (x)
+  (cond
+    ((null? x) 0)
+    (else (add1 (length (cdr x)))))))
+
+(eq? 3 (length '(1 2 3)))
+(eq? 0 (length '()))
+
+(define pick
+  (lambda (n lat)
+    (cond
+      ((eq? 1 n) (car lat))
+      (else (pick (sub1 n) (cdr lat))))))
+
+(eq? 2 (pick 2 '(1 2 3 4)))
+(eq? 1 (pick 1 '(1 2 3 4)))
