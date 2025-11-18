7752726b45e601f3967a25701da0a0ddaaa2ac6f
cafewang <434637878@qq.com>
2025-11-18 22:47:52 +0800

    length from scatch(almost done)

--- length_from_scratch.rkt
+++ length_from_scratch.rkt
@@ -65,3 +65,52 @@
         (cond
           ((null? l) 0)
           (else (add1 (length (cdr l)))))))) '(a b c))
+
+
+; yet another length0, inner length now take (car l) as parameter and return a function (lambda l) which the else branch won't compile, becuase it applys (cdr l) to (cdr l)
+(((lambda (mklength) (mklength mklength))
+    (lambda (length)
+      (lambda (l)
+        (cond
+         ((null? l) 0)
+         (else (add1 (length (cdr l)))))))) '())
+
+; make length<=1 by length0
+(((lambda (mklength) (mklength (mklength mklength)))
+    (lambda (length)
+      (lambda (l)
+        (cond
+         ((null? l) 0)
+         (else (add1 (length (cdr l)))))))) '(a))
+
+; the same as the above one
+(((lambda (length)
+      (lambda (l)
+        (cond
+         ((null? l) 0)
+         (else (add1 ((length length) (cdr l)))))))
+    (lambda (length)
+      (lambda (l)
+        (cond
+         ((null? l) 0)
+         (else (add1 (length (cdr l)))))))) '(a))
+
+; we want to make it non-stop, try the one below
+(((lambda (mklength) (mklength mklength))
+    (lambda (mklength)
+      (lambda (l)
+        (cond
+          ((null? l) 0)
+          (else (add1 ((mklength mklength) (cdr l)))))))) '(a b c d e))
+
+; the same as the above one, (mklength mklength) expands to the whole expression
+(((lambda (mklength)
+    (lambda (l)
+      (cond
+        ((null? l) 0)
+        (else (add1 ((mklength mklength) (cdr l)))))))
+  (lambda (mklength)
+    (lambda (l)
+      (cond
+        ((null? l) 0)
+        (else (add1 ((mklength mklength) (cdr l)))))))) '(a b c d e))
