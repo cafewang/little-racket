0e597d145ada3836fc28fe28e5598de1b1f3ae0c
cafewang <434637878@qq.com>
2025-11-20 00:14:33 +0800

    complete Y combinator

--- length_from_scratch.rkt
+++ length_from_scratch.rkt
@@ -114,3 +114,60 @@
       (cond
         ((null? l) 0)
         (else (add1 ((mklength mklength) (cdr l)))))))) '(a b c d e))
+
+; here we are again
+(((lambda (mklength) (mklength mklength))
+    (lambda (mklength)
+      (lambda (l)
+        (cond
+          ((null? l) 0)
+          (else (add1 ((mklength mklength) (cdr l)))))))) '(a b c d e))
+
+; let's extract out (mklength mklength)
+; (((lambda (mklength) (mklength mklength))
+;     (lambda (mklength)
+;       ((lambda (length)
+;         (lambda (l)
+;           (cond
+;             ((null? l) 0)
+;             (else (add1 (length (cdr l))))))) (mklength mklength)))) '(a b c d e))
+
+; sadly, above code won't work for racket, let's try something else
+(((lambda (mklength) (mklength mklength))
+    (lambda (mklength)
+      ((lambda (length)
+        (lambda (l)
+          (cond
+            ((null? l) 0)
+            (else (add1 (length (cdr l))))))) (lambda (x) ((mklength mklength) x))))) '(a b c d e))
+
+; let's extract lambda (l) as well
+(((lambda (recursive-part) ((lambda (mklength) (mklength mklength))
+    (lambda (mklength)
+      (recursive-part (lambda (x) ((mklength mklength) x))))))
+    (lambda (length)
+          (lambda (l)
+            (cond
+              ((null? l) 0)
+              (else (add1 (length (cdr l)))))))) '(a b c d e))
+
+; simplify symbols
+(((lambda (rp) ((lambda (f) (f f))
+    (lambda (f)
+      (rp (lambda (x) ((f f) x))))))
+    (lambda (length)
+          (lambda (l)
+            (cond
+              ((null? l) 0)
+              (else (add1 (length (cdr l)))))))) '(a b c d e))
+
+; try fibonacci
+(((lambda (rp) ((lambda (f) (f f))
+    (lambda (f)
+      (rp (lambda (x) ((f f) x))))))
+    (lambda (fibonacci)
+          (lambda (n)
+            (cond
+              ((zero? n) 0)
+              ((= 1 n) 1)
+              (else (+ (fibonacci (- n 1)) (fibonacci (- n 2)))))))) 5)
